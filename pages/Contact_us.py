4bee8c582a40cded9ce79b28c943b056c3582c2a
CodeSolaris <aleksandr.volkov.it@gmail.com>
2023-11-10 23:58:08 +0200

    clean up the code

--- pages/Contact_us.py
+++ pages/Contact_us.py
@@ -3,12 +3,10 @@ import pandas as pd
 from send_to_email import send_email
 
 
-st.set_page_config(layout="wide")
 with open("topics.csv") as db_file:
     db = pd.read_csv(db_file)
-    options = db["topic"].to_list()
+    options = db["topic"]
 
-st.sidebar.header("Contact Us")
 
 st.title("Contact Us")
 st.write("Please fill out the form below to send me a message.")
@@ -17,9 +15,11 @@ with st.form(key="contact_form"):
     sender_email = st.text_input("Your email")
     selected_topic = st.selectbox("Choose a topic", options)
     raw_message = st.text_area("Your message")
-    message=f"""\
-Subject: {selected_topic}
+    message = f"""\
+Subject: New email from {sender_email} 
+
 From: {sender_email}
+Topic: {selected_topic}
 {raw_message}
 
 """
